e4629e51e02b5256f4aeaccaba98f1811a4b73e8
emrah <emrah.com@gmail.com>
2021-10-10 19:44:33 +0300

    feat, eb-jitsi: jitsi-meet dev environment

---
+++ installer-sub-scripts/eb-jitsi/306-jitsi-meet-dev.sh
@@ -0,0 +1,136 @@
+# -----------------------------------------------------------------------------
+# JITSI-MEET-DEV.SH
+# -----------------------------------------------------------------------------
+set -e
+source $INSTALLER/000-source
+
+# -----------------------------------------------------------------------------
+# ENVIRONMENT
+# -----------------------------------------------------------------------------
+MACH="eb-jitsi"
+cd $MACHINES/$MACH
+
+ROOTFS="/var/lib/lxc/$MACH/rootfs"
+
+# -----------------------------------------------------------------------------
+# INIT
+# -----------------------------------------------------------------------------
+[ "$INSTALL_JITSI_MEET_DEV" != true ] && exit
+
+echo
+echo "---------------------- JITSI MEET DEV ---------------------"
+
+# -----------------------------------------------------------------------------
+# CONTAINER
+# -----------------------------------------------------------------------------
+# start container
+lxc-start -n $MACH -d
+lxc-wait -n $MACH -s RUNNING
+
+# wait for the network to be up
+for i in $(seq 0 9); do
+    lxc-attach -n $MACH -- ping -c1 host && break || true
+    sleep 1
+done
+
+# -----------------------------------------------------------------------------
+# PACKAGES
+# -----------------------------------------------------------------------------
+# fake install
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY_OPTION -dy reinstall hostname
+EOS
+
+# nodejs repo
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+EOS
+
+cp etc/apt/sources.list.d/nodesource.list $ROOTFS/etc/apt/sources.list.d/
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+apt-get $APT_PROXY_OPTION update
+EOS
+
+# packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY_OPTION -y install gnupg git build-essential
+apt-get $APT_PROXY_OPTION -y install nodejs
+EOS
+
+# -----------------------------------------------------------------------------
+# JITSI-MEET DEV
+# -----------------------------------------------------------------------------
+# store folder
+mkdir -p /root/eb-store
+
+# dev folder
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+mkdir -p /home/dev
+cd /home/dev
+EOS
+
+# lib-jitsi-meet
+if [[ ! -d /root/eb-store/lib-jitsi-meet ]]; then
+    git clone --depth=200 -b master \
+        https://github.com/jitsi/lib-jitsi-meet.git \
+        /root/eb-store/lib-jitsi-meet
+fi
+
+zsh <<EOS
+set -e
+cd /root/eb-store/lib-jitsi-meet
+git pull
+EOS
+
+rm -rf $ROOTFS/home/dev/lib-jitsi-meet
+cp -arp /root/eb-store/lib-jitsi-meet $ROOTFS/home/dev/
+
+# jitsi-meet
+if [[ ! -d /root/eb-store/jitsi-meet ]]; then
+    git clone --depth=200 -b master https://github.com/jitsi/jitsi-meet.git \
+        /root/eb-store/jitsi-meet
+fi
+
+zsh <<EOS
+set -e
+cd /root/eb-store/jitsi-meet
+git pull
+EOS
+
+rm -rf $ROOTFS/home/dev/jitsi-meet
+cp -arp /root/eb-store/jitsi-meet $ROOTFS/home/dev/
+
+# -----------------------------------------------------------------------------
+# SYSTEM CONFIGURATION
+# -----------------------------------------------------------------------------
+# nginx
+cp $ROOTFS/etc/nginx/sites-available/$JITSI_FQDN.conf \
+    $ROOTFS/etc/nginx/sites-available/$JITSI_FQDN-dev.conf
+sed -i "s~/usr/share/jitsi-meet~/home/dev/jitsi-meet~g" \
+    $ROOTFS/etc/nginx/sites-available/$JITSI_FQDN-dev.conf
+
+# dev tools
+cp usr/local/sbin/enable-jitsi-meet-dev $ROOTFS/usr/local/sbin/
+cp usr/local/sbin/disable-jitsi-meet-dev $ROOTFS/usr/local/sbin/
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" \
+    $ROOTFS/usr/local/sbin/enable-jitsi-meet-dev
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" \
+    $ROOTFS/usr/local/sbin/disable-jitsi-meet-dev
+chmod 744 $ROOTFS/usr/local/sbin/enable-jitsi-meet-dev
+chmod 744 $ROOTFS/usr/local/sbin/disable-jitsi-meet-dev
+
+# -----------------------------------------------------------------------------
+# SYSTEM INFO
+# -----------------------------------------------------------------------------
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+node --version
+npm --version
+EOS
