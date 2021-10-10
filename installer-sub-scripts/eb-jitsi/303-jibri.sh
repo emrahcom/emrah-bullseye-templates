2f7fb46b9f3a0d2510db16865efbac808815a2cd
emrah <emrah.com@gmail.com>
2021-10-10 15:19:10 +0300

    feat, eb-jitsi: jibri container

--- installer-sub-scripts/eb-jitsi/303-jibri.sh
+++ installer-sub-scripts/eb-jitsi/303-jibri.sh
@@ -74,7 +74,7 @@ lxc.start.auto = 1
 lxc.start.order = 303
 lxc.start.delay = 2
 lxc.group = eb-group
-lxc.group = onboot
+lxc.group = eb-jitsi
 EOF
 
 # dhcp config
@@ -101,6 +101,15 @@ hostname $MACH
 EOS
 
 # -----------------------------------------------------------------------------
+# HOST PACKAGES
+# -----------------------------------------------------------------------------
+zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY_OPTION -y install kmod alsa-utils
+EOS
+
+# -----------------------------------------------------------------------------
 # PACKAGES
 # -----------------------------------------------------------------------------
 # fake install
@@ -118,6 +127,234 @@ apt-get $APT_PROXY_OPTION update
 apt-get $APT_PROXY_OPTION -y dist-upgrade
 EOS
 
+# packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY_OPTION -y install apt-transport-https gnupg
+apt-get $APT_PROXY_OPTION -y install libnss3-tools
+apt-get $APT_PROXY_OPTION -y install va-driver-all vdpau-driver-all
+apt-get $APT_PROXY_OPTION -y --install-recommends install ffmpeg
+apt-get $APT_PROXY_OPTION -y --install-recommends install chromium \
+    chromium-driver
+apt-get $APT_PROXY_OPTION -y install stunnel x11vnc
+EOS
+
+# jibri
+cp etc/apt/sources.list.d/jitsi-stable.list $ROOTFS/etc/apt/sources.list.d/
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+wget -qO /tmp/jitsi.gpg.key https://download.jitsi.org/jitsi-key.gpg.key
+cat /tmp/jitsi.gpg.key | gpg --dearmor >/usr/share/keyrings/jitsi-keyring.gpg
+apt-get update
+EOS
+
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY_OPTION -y install jibri
+EOS
+
+# removed packages
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y purge upower
+apt-get -y --purge autoremove
+EOS
+
+# -----------------------------------------------------------------------------
+# SYSTEM CONFIGURATION
+# -----------------------------------------------------------------------------
+# disable ssh service
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+systemctl stop ssh.service
+systemctl disable ssh.service
+EOS
+
+# jitsi host
+echo -e "$JITSI\t$JITSI_FQDN" >> $ROOTFS/etc/hosts
+
+# certificates
+cp /root/eb_ssl/eb-CA.pem $ROOTFS/usr/local/share/ca-certificates/jms-CA.crt
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+update-ca-certificates
+EOS
+
+# snd_aloop module
+[ -z "$(egrep '^snd_aloop' /etc/modules)" ] && echo snd_aloop >>/etc/modules
+cp $MACHINES/eb-jitsi-host/etc/modprobe.d/alsa-loopback.conf /etc/modprobe.d/
+rmmod -f snd_aloop || true
+modprobe snd_aloop || true
+[[ "$DONT_CHECK_SND_ALOOP" = true ]] || [[ -n "$(lsmod | ack snd_aloop)" ]]
+
+# chromium managed policies
+mkdir -p $ROOTFS/etc/chromium/policies/managed
+cp etc/chromium/policies/managed/eb_policies.json \
+    $ROOTFS/etc/chromium/policies/managed/
+
+# stunnel
+cp etc/stunnel/facebook.conf $ROOTFS/etc/stunnel/
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+systemctl start stunnel4.service
+EOS
+
+# -----------------------------------------------------------------------------
+# JITSI CUSTOMIZATION FOR JIBRI
+# -----------------------------------------------------------------------------
+# prosody config
+sed -i '/^VirtualHost.*"recorder\..*"/, /authentication/d' \
+    $JITSI_ROOTFS/etc/prosody/conf.avail/$JITSI_FQDN.cfg.lua
+cat >> $JITSI_ROOTFS/etc/prosody/conf.avail/$JITSI_FQDN.cfg.lua <<EOF
+VirtualHost "recorder.$JITSI_FQDN"
+    modules_enabled = {
+      "ping";
+    }
+    authentication = "internal_plain"
+EOF
+
+# prosody register
+JIBRI_DAT=$(find $JITSI_ROOTFS/var/lib/prosody -name 'jibri.dat')
+[[ -z "$JIBRI_DAT" ]] && \
+    PASSWD1=$(echo -n $RANDOM$RANDOM | sha256sum | cut -c 1-20) || \
+    PASSWD1=$(grep password $JIBRI_DAT | cut -d '"' -f4)
+RECORDER_DAT=$(find $JITSI_ROOTFS/var/lib/prosody -name 'recorder.dat')
+[[ -z "$RECORDER_DAT" ]] && \
+    PASSWD2=$(echo -n $RANDOM$RANDOM | sha256sum | cut -c 1-20) || \
+    PASSWD2=$(grep password $RECORDER_DAT | cut -d '"' -f4)
+
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+systemctl restart prosody.service
+EOS
+
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+prosodyctl unregister jibri auth.$JITSI_FQDN || true
+prosodyctl register jibri auth.$JITSI_FQDN $PASSWD1
+prosodyctl unregister recorder recorder.$JITSI_FQDN || true
+prosodyctl register recorder recorder.$JITSI_FQDN $PASSWD2
+EOS
+
+# jicofo config
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+hocon -f /etc/jitsi/jicofo/jicofo.conf \
+    set jicofo.jibri.brewery-jid "\"JibriBrewery@internal.auth.$JITSI_FQDN\""
+hocon -f /etc/jitsi/jicofo/jicofo.conf \
+    set jicofo.jibri.pending-timeout "90 seconds"
+EOS
+
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+systemctl restart jicofo.service
+EOS
+
+# jitsi-meet config
+sed -i 's~//\s*fileRecordingsEnabled.*~fileRecordingsEnabled: true,~' \
+    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+sed -i 's~//\s*fileRecordingsServiceSharingEnabled.*~fileRecordingsServiceSharingEnabled: true,~' \
+    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+sed -i 's~//\s*liveStreamingEnabled:.*~liveStreamingEnabled: true,~' \
+    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+sed -i "/liveStreamingEnabled:/a \\\n    hiddenDomain: 'recorder.$JITSI_FQDN'," \
+    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+
+# -----------------------------------------------------------------------------
+# JIBRI SSH KEY
+# -----------------------------------------------------------------------------
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+# create ssh key if not exists
+if [[ ! -f /root/.ssh/jibri ]] || [[ ! -f /root/.ssh/jibri.pub ]]; then
+    rm -f /root/.ssh/jibri{,.pub}
+    ssh-keygen -qP '' -t rsa -b 2048 -f /root/.ssh/jibri
+fi
+
+# copy the public key to a downloadable place
+cp /root/.ssh/jibri.pub $JITSI_ROOTFS/usr/share/jitsi-meet/static/
+
+# -----------------------------------------------------------------------------
+# JIBRI
+# -----------------------------------------------------------------------------
+# jibri groups
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+usermod -aG adm,audio,video,plugdev jibri
+EOS
+
+# jibri ssh
+mkdir -p $ROOTFS/home/jibri/.ssh
+chmod 700 $ROOTFS/home/jibri/.ssh
+cp home/jibri/.ssh/jibri-config $ROOTFS/home/jibri/.ssh/
+cp /root/.ssh/jibri $ROOTFS/home/jibri/.ssh/
+
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+chown jibri:jibri /home/jibri/.ssh -R
+EOS
+
+# recordings directory
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+chown jibri:jibri /usr/local/eb/recordings -R
+EOS
+
+# pki
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+mkdir -p /home/jibri/.pki/nssdb
+chmod 700 /home/jibri/.pki
+chmod 700 /home/jibri/.pki/nssdb
+
+certutil -A -n "jitsi" -i /usr/local/share/ca-certificates/jms-CA.crt \
+    -t "TCu,Cu,Tu" -d sql:/home/jibri/.pki/nssdb/
+chown jibri:jibri /home/jibri/.pki -R
+EOS
+
+# jibri config
+cp etc/jitsi/jibri/jibri.conf $ROOTFS/etc/jitsi/jibri/
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" $ROOTFS/etc/jitsi/jibri/jibri.conf
+sed -i "s/___PASSWD1___/$PASSWD1/" $ROOTFS/etc/jitsi/jibri/jibri.conf
+sed -i "s/___PASSWD2___/$PASSWD2/" $ROOTFS/etc/jitsi/jibri/jibri.conf
+
+# the customized scripts
+cp usr/local/bin/finalize_recording.sh $ROOTFS/usr/local/bin/
+chmod 755 $ROOTFS/usr/local/bin/finalize_recording.sh
+cp usr/local/bin/ffmpeg $ROOTFS/usr/local/bin/
+chmod 755 $ROOTFS/usr/local/bin/ffmpeg
+
+# jibri ephemeral config service
+cp usr/local/sbin/jibri-ephemeral-config $ROOTFS/usr/local/sbin/
+chmod 744 $ROOTFS/usr/local/sbin/jibri-ephemeral-config
+cp etc/systemd/system/jibri-ephemeral-config.service \
+    $ROOTFS/etc/systemd/system/
+
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+systemctl daemon-reload
+systemctl enable jibri-ephemeral-config.service
+EOS
+
+# jibri service
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+systemctl enable jibri.service
+systemctl start jibri.service
+EOS
+
+# jibri vnc
+lxc-attach -n eb-jitsi -- zsh <<EOS
+set -e
+mkdir -p /home/jibri/.vnc
+x11vnc -storepasswd jibri /home/jibri/.vnc/passwd
+chown jibri:jibri /home/jibri/.vnc -R
+EOS
+
 # -----------------------------------------------------------------------------
 # CONTAINER SERVICES
 # -----------------------------------------------------------------------------
@@ -129,3 +366,23 @@ lxc-wait -n $MACH -s STOPPED
 # CLEAN UP
 # -----------------------------------------------------------------------------
 find $ROOTFS/var/log/jitsi/jibri -type f -delete
+
+# -----------------------------------------------------------------------------
+# HOST CUSTOMIZATION FOR JIBRI
+# -----------------------------------------------------------------------------
+# jitsi tools
+cp $MACHINES/eb-jitsi-host/usr/local/sbin/add-jibri-node /usr/local/sbin/
+chmod 744 /usr/local/sbin/add-jibri-node
+
+# jibri-ephemeral-container service
+cp $MACHINES/eb-jitsi-host/usr/local/sbin/jibri-ephemeral-start /usr/local/sbin/
+cp $MACHINES/eb-jitsi-host/usr/local/sbin/jibri-ephemeral-stop /usr/local/sbin/
+chmod 744 /usr/local/sbin/jibri-ephemeral-start
+chmod 744 /usr/local/sbin/jibri-ephemeral-stop
+
+cp $MACHINES/eb-jitsi-host/etc/systemd/system/jibri-ephemeral-container.service \
+    /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable jibri-ephemeral-container.service
+systemctl start jibri-ephemeral-container.service
