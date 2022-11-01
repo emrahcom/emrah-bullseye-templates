5af42864d9fc2bf9efac6f8d36076ca471c53c9c
emrah <emrah.eryilmaz@nordeck.net>
2022-11-01 17:41:29 +0300

    refactor: renamed APT_PROXY in sip-dial-plan scripts

--- installer-sub-scripts/eb-jitsi/307-sip-dial-plan.sh
+++ installer-sub-scripts/eb-jitsi/307-sip-dial-plan.sh
@@ -93,22 +93,22 @@ done
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get $APT_PROXY_OPTION -dy reinstall hostname
+apt-get $APT_PROXY -dy reinstall hostname
 EOS
 
 # update
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get $APT_PROXY_OPTION update
-apt-get $APT_PROXY_OPTION -y dist-upgrade
+apt-get $APT_PROXY update
+apt-get $APT_PROXY -y dist-upgrade
 EOS
 
 # packages
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get $APT_PROXY_OPTION -y install unzip git
+apt-get $APT_PROXY -y install unzip git
 EOS
 
 # ------------------------------------------------------------------------------
