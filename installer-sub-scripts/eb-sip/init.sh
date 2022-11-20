25380bc0d19b1a9bfa8a2aebfbb7f539346d56b6
emrah <emrah.eryilmaz@nordeck.net>
2022-11-20 17:35:55 +0300

    eb-sip, refactor: stop ephemeral at init.sh

--- installer-sub-scripts/eb-sip/init.sh
+++ installer-sub-scripts/eb-sip/init.sh
@@ -19,3 +19,7 @@ cd $INSTALLER
 # INSTALLER CONFIGURATION
 # ------------------------------------------------------------------------------
 cp -ap ../eb-base/* .
+
+set +e
+systemctl stop sip-ephemeral-container.service
+set -e
