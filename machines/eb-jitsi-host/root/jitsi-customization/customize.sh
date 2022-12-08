8e168f10a8d612af8162159e5d93ce7819491fb8
emrah <emrah.eryilmaz@nordeck.net>
2022-12-08 20:32:45 +0300

    eb-jitsi-host, refactor: simplify customization script

--- machines/eb-jitsi-host/root/jitsi-customization/customize.sh
+++ machines/eb-jitsi-host/root/jitsi-customization/customize.sh
@@ -12,7 +12,6 @@ JITSI_ROOTFS="/var/lib/lxc/eb-jitsi/rootfs"
 JITSI_MEET_CONFIG="$JITSI_ROOTFS/etc/jitsi/meet/___JITSI_FQDN___-config.js"
 JITSI_MEET_INTERFACE="$JITSI_ROOTFS/usr/share/jitsi-meet/interface_config.js"
 JITSI_MEET_APP="$JITSI_ROOTFS/usr/share/jitsi-meet"
-PROSODY_CONFIG="$JITSI_ROOTFS/etc/prosody/conf.avail/___JITSI_FQDN___.cfg.lua"
 
 # ------------------------------------------------------------------------------
 # config.js
@@ -35,40 +34,3 @@ PROSODY_CONFIG="$JITSI_ROOTFS/etc/prosody/conf.avail/___JITSI_FQDN___.cfg.lua"
     cp $BASEDIR/files/favicon.ico $JITSI_MEET_APP/images/
 [[ -f "$BASEDIR/files/watermark.svg" ]] && \
     cp $BASEDIR/files/watermark.svg $JITSI_MEET_APP/images/
-
-# ------------------------------------------------------------------------------
-# jwt
-# ------------------------------------------------------------------------------
-# Set disableProfile in local config.js if needed
-
-#APP_ID="myappid"
-#APP_SECRET="myappsecret"
-
-#lxc-attach -n eb-jitsi -- zsh <<EOS
-#set -e
-#export DEBIAN_FRONTEND=noninteractive
-#debconf-set-selections <<< \
-#    'jitsi-meet-tokens jitsi-meet-tokens/appid string $APP_ID'
-#debconf-set-selections <<< \
-#    'jitsi-meet-tokens jitsi-meet-tokens/appsecret password $APP_SECRET'
-#apt-get -y --allow-change-held-packages install jitsi-meet-tokens
-#EOS
-
-#sed -i '/allow_empty_token/d' $PROSODY_CONFIG
-#sed -i '/token_affiliation/d' $PROSODY_CONFIG
-#sed -i '/token_owner_party/d' $PROSODY_CONFIG
-#sed -i '/\s*app_secret=/a \
-#\    allow_empty_token = false' $PROSODY_CONFIG
-#sed -i '/^Component .conference\./,/admins/!b; /\s*"token_verification"/a \
-#\        "token_affiliation";' $PROSODY_CONFIG
-#sed -i '/^Component .conference\./,/admins/!b; /\s*"token_affiliation"/a \
-#\        "token_owner_party";' $PROSODY_CONFIG
-#lxc-attach -n eb-jitsi -- systemctl restart prosody.service
-
-#lxc-attach -n eb-jitsi -- zsh <<EOS
-#set -e
-#hocon -f /etc/jitsi/jicofo/jicofo.conf \
-#    set jicofo.conference.enable-auto-owner false
-#systemctl restart jicofo.service
-#systemctl restart jitsi-videobridge2.service
-#EOS
