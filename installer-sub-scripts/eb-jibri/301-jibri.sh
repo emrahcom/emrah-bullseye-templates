c9245a0c89c618b3c03986d96e2ccfbdad1f4bd3
emrah <emrah.eryilmaz@nordeck.net>
2022-12-09 18:22:07 +0300

    eb-jibri, refactor: update jibri.sh

--- installer-sub-scripts/eb-jibri/301-jibri.sh
+++ installer-sub-scripts/eb-jibri/301-jibri.sh
@@ -113,6 +113,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get $APT_PROXY -y install gnupg unzip unclutter
 apt-get $APT_PROXY -y install libnss3-tools
 apt-get $APT_PROXY -y install va-driver-all vdpau-driver-all
+apt-get $APT_PROXY -y install openjdk-11-jre-headless
 apt-get $APT_PROXY -y --install-recommends install ffmpeg
 apt-get $APT_PROXY -y install x11vnc
 EOS
@@ -123,7 +124,8 @@ lxc-attach -n $MACH -- zsh <<EOS
 set -e
 wget -T 30 -qO /tmp/google-chrome.gpg.key \
     https://dl.google.com/linux/linux_signing_key.pub
-apt-key add /tmp/google-chrome.gpg.key
+cat /tmp/google-chrome.gpg.key | gpg --dearmor \
+    >/usr/share/keyrings/google-chrome.gpg
 apt-get $APT_PROXY update
 EOS
 
@@ -132,6 +134,16 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 apt-get $APT_PROXY -y --install-recommends install google-chrome-stable
 apt-mark hold google-chrome-stable
+EOS
+
+# fix overwritten google-chrome sources list by recopying it
+# google tries to add its key as a globally trusted one, limit its permissions
+cp etc/apt/sources.list.d/google-chrome.list $ROOTFS/etc/apt/sources.list.d/
+
+lxc-attach -n $MACH -- zsh <<EOS
+set -e
+rm -f /etc/apt/trusted.gpg.d/google-chrome.*
+apt-get $APT_PROXY update
 EOS
 
 # chromedriver
@@ -153,17 +165,17 @@ lxc-attach -n $MACH -- zsh <<EOS
 set -e
 wget -T 30 -qO /tmp/jitsi.gpg.key https://download.jitsi.org/jitsi-key.gpg.key
 cat /tmp/jitsi.gpg.key | gpg --dearmor >/usr/share/keyrings/jitsi.gpg
-apt-get update
+apt-get $APT_PROXY update
 EOS
 
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get $APT_PROXY -y install openjdk-11-jre-headless
 
 [[ -z "$JIBRI_VERSION" ]] && \
     apt-get $APT_PROXY -y install jibri || \
     apt-get $APT_PROXY -y install jibri=$JIBRI_VERSION
+
 apt-mark hold jibri
 EOS
 
@@ -184,13 +196,6 @@ systemctl stop ssh.service
 systemctl disable ssh.service
 EOS
 
-# snd_aloop module
-[[ -z "$(egrep '^snd_aloop' /etc/modules)" ]] && echo snd_aloop >>/etc/modules
-cp $MACHINES/$TAG-jibri-host/etc/modprobe.d/alsa-loopback.conf /etc/modprobe.d/
-rmmod -f snd_aloop || true
-modprobe snd_aloop || true
-[[ "$DONT_CHECK_SND_ALOOP" = true ]] || [[ -n "$(lsmod | ack snd_aloop)" ]]
-
 # google chrome managed policies
 mkdir -p $ROOTFS/etc/opt/chrome/policies/managed
 cp etc/opt/chrome/policies/managed/$TAG-policies.json \
@@ -201,14 +206,6 @@ cp etc/opt/chrome/policies/managed/$TAG-policies.json \
 # ------------------------------------------------------------------------------
 cp $ROOTFS/etc/jitsi/jibri/xorg-video-dummy.conf \
     $ROOTFS/etc/jitsi/jibri/xorg-video-dummy.conf.org
-
-# meta
-lxc-attach -n $MACH -- zsh <<EOS
-set -e
-mkdir -p /root/meta
-VERSION=\$(apt-cache policy jibri | grep Installed | rev | cut -d' ' -f1 | rev)
-echo \$VERSION > /root/meta/jibri-version
-EOS
 
 # jibri groups
 lxc-attach -n $MACH -- zsh <<EOS
@@ -222,9 +219,10 @@ EOS
 mkdir -p $ROOTFS/home/jibri/.ssh
 chmod 700 $ROOTFS/home/jibri/.ssh
 cp home/jibri/.ssh/jibri-config $ROOTFS/home/jibri/.ssh/
-[[ -f /root/.ssh/jibri ]] && \
-    cp /root/.ssh/jibri $ROOTFS/home/jibri/.ssh/ || \
-    true
+
+if [[ -f /root/.ssh/jibri ]]; then
+    cp /root/.ssh/jibri $ROOTFS/home/jibri/.ssh/
+fi
 
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
@@ -245,16 +243,32 @@ chown jibri:jibri /usr/local/$TAG/recordings -R
 EOS
 
 # pki
+if [[ -f /root/.ssh/jms-CA.crt ]]; then
+    cp /root/.ssh/jms-CA.crt $ROOTFS/usr/local/share/ca-certificates/
+fi
+
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
+update-ca-certificates
+
 mkdir -p /home/jibri/.pki/nssdb
 chmod 700 /home/jibri/.pki
 chmod 700 /home/jibri/.pki/nssdb
+
+if [[ -f "/usr/local/share/ca-certificates/jms-CA.crt" ]]; then
+    certutil -A -n 'jitsi' -i /usr/local/share/ca-certificates/jms-CA.crt \
+        -t 'TCu,Cu,Tu' -d sql:/home/jibri/.pki/nssdb/
+fi
+
 chown jibri:jibri /home/jibri/.pki -R
 EOS
 
 # jibri config
 cp etc/jitsi/jibri/jibri.conf $ROOTFS/etc/jitsi/jibri/
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" $ROOTFS/etc/jitsi/jibri/jibri.conf
+sed -i "s/___JIBRI_PASSWD___/$JIBRI_PASSWD/" $ROOTFS/etc/jitsi/jibri/jibri.conf
+sed -i "s/___RECORDER_PASSWD___/$RECORDER_PASSWD/" \
+    $ROOTFS/etc/jitsi/jibri/jibri.conf
 
 # the customized scripts
 cp usr/local/bin/finalize-recording.sh $ROOTFS/usr/local/bin/
@@ -307,3 +321,10 @@ lxc-wait -n $MACH -s STOPPED
 # CLEAN UP
 # ------------------------------------------------------------------------------
 find $ROOTFS/var/log/jitsi/jibri -type f -delete
+
+# ------------------------------------------------------------------------------
+# ON HOST
+# ------------------------------------------------------------------------------
+systemctl daemon-reload
+systemctl enable jibri-ephemeral-container.service
+systemctl start jibri-ephemeral-container.service
