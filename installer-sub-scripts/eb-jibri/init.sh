d64d2dcba145d00b37576b00eca8469b109fc780
emrah <emrah.eryilmaz@nordeck.net>
2022-12-09 18:10:33 +0300

    eb-jibri, refactor: checks in init.sh

--- installer-sub-scripts/eb-jibri/init.sh
+++ installer-sub-scripts/eb-jibri/init.sh
@@ -16,9 +16,19 @@ cd $INSTALLER
 [[ "$DONT_RUN_INIT" = true ]] && exit
 
 # ------------------------------------------------------------------------------
+# CHECKS
+# ------------------------------------------------------------------------------
+echo
+
+[[ -z "$JITSI_FQDN" ]] && echo "JITSI_FQDN not found" && false
+[[ -z "$(dig +short $JITSI_FQDN)" ]] && echo "unresolvable JITSI_FQDN" && false
+[[ -z "$JIBRI_PASSWD" ]] && echo "JIBRI_PASSWD not found" && false
+[[ -z "$RECORDER_PASSWD" ]] && echo "RECORDER_PASSWD not found" && false
+
+# ------------------------------------------------------------------------------
 # INSTALLER CONFIGURATION
 # ------------------------------------------------------------------------------
-cp -ap ../eb-base/* .
+cp -ap ../$TAG-base/* .
 
 set +e
 systemctl stop jibri-ephemeral-container.service
