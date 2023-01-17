a00521a6d4fb2dce9f8a019ee6c226cdc0bd7ce7
emrah <emrah.eryilmaz@nordeck.net>
2023-01-17 10:44:01 +0300

    refactor: add $CONFIG_JS

--- installer-sub-scripts/eb-jitsi/308-jitsi-customization.sh
+++ installer-sub-scripts/eb-jitsi/308-jitsi-customization.sh
@@ -11,6 +11,7 @@ MACH="$TAG-jitsi-host"
 cd $MACHINES/$MACH
 
 JITSI_ROOTFS="/var/lib/lxc/$TAG-jitsi/rootfs"
+CONFIG_JS="$JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js"
 
 # ------------------------------------------------------------------------------
 # INIT
@@ -24,20 +25,16 @@ echo "------------------- JITSI CUSTOMIZATION -------------------"
 # CONFIG.JS
 # ------------------------------------------------------------------------------
 # recording
-cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.recording.js >> \
-    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.recording.js >> $CONFIG_JS
 
 # livestreaming
-cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.livestreaming.js >> \
-    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.livestreaming.js >> $CONFIG_JS
 
 # UI customizations
-cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.ui.js >> \
-    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+cat $MACHINES/$TAG-jitsi/etc/jitsi/meet/config.ui.js >> $CONFIG_JS
 
 # substitutions
-sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" \
-    $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/" $CONFIG_JS
 
 # ------------------------------------------------------------------------------
 # CUSTOMIZATION FOLDER & TOOLS
@@ -61,7 +58,7 @@ sed -i "s/___TURN_FQDN___/$TURN_FQDN/g" $FOLDER/customize.sh
 sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/g" $FOLDER/customize.sh
 
 mkdir -p $FOLDER/files
-cp $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js $FOLDER/files/
+cp $CONFIG_JS $FOLDER/files/
 cp $JITSI_ROOTFS//usr/share/jitsi-meet/interface_config.js $FOLDER/files/
 cp $JITSI_ROOTFS/usr/share/jitsi-meet/images/favicon.ico $FOLDER/files/
 cp $JITSI_ROOTFS/usr/share/jitsi-meet/images/watermark.svg $FOLDER/files/
